3a7b5f237b47fc6a3d6ac830b74fa58d8afdf11d
akkenji000 <akkenji12345@gmail.com>
2025-09-14 20:16:55 -0300

    Update Emprestimo.java
    Desenvolvimento classe emprestimo, com atributos, constructor e métodos adicionados

--- src/Emprestimo.java
+++ src/Emprestimo.java
@@ -1,4 +1,22 @@
+import java.time.LocalDate; //import de data
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class Emprestimo {
     private Usuario usuario;
     private Livro livro;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataDevolucaoPrevista;
+    private LocalDate dataDevolucaoReal;
+
+    public Emprestimo(Livro livro, Usuario usuario){
+        this.livro = livro;
+        this.usuario = usuario;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucaoPrevista = dataEmprestimo.plusDays(usuario.calcularPrazoDevolucao());
+        this.dataDevolucaoReal = dataDevolucaoReal;
+    }
+
+    public void finalizarEmprestimo(){
+        this.dataDevolucaoReal = LocalDate.now();
+    }
 }
